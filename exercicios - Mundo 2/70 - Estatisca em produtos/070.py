9110fffc82bc4f8d5e9cb46001e40792a058a6db
ledsonJyan <jyancastrojl@gmail.com>
2025-03-10 01:48:47 -0300

    exercico 070

--- exercicios - Mundo 2/70 - Estatisca em produtos/070.py
+++ exercicios - Mundo 2/70 - Estatisca em produtos/070.py
@@ -1,4 +1,5 @@
-produto = valor = escolha = soma = cont = 0
+produto = valor = escolha = soma = cont = menor = 0
+barato = ''
 while True:
     produto = input('Qual o nome do produto?')
     valor = float(input('Qual o valor?'))
@@ -7,10 +8,15 @@ while True:
         print('Resposta invalida')
         continue
     soma += valor
-    
     if valor>1000:
       cont += 1
+    
+    if menor == 0 or valor < menor:  
+        menor = valor
+        barato = produto
     if escolha == 'N':
         break
+
 print(f'O total ficou: {soma}')
 print(f'Tem {cont} produtos acima de 1000')
+print(f'O produto mais barato é: {barato} e vale: {menor}')
